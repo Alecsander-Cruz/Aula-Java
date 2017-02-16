3b534e6e25a805da2395a0185b0a56fce9e6817f
Alecsander-Cruz <alecsandercruz@hotmail.com>
2017-02-15 21:28:08 -0300

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,23 @@
+package br.unipe.java.unidade1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Vrum ferrari = new Vrum();
+		ferrari.retroceder();
+		ferrari.avançar();
+		ferrari.parar();
+		ferrari.abastercer();
+		
+		
+		Vrum fusca = new Vrum();
+		fusca.retroceder();
+		fusca.avançar();
+		fusca.parar();
+		fusca.abastercer();
+		
+
+	}
+
+}
