b7e7e9e6a10193aea53ad0bcb4650afe22d7b584
Alecsander-Cruz <alecsandercruz@hotmail.com>
2017-02-16 21:46:09 -0300

    Add files via upload

---
+++ Canil.java
@@ -0,0 +1,22 @@
+package br.unipe.java.unidade1;
+
+public class Canil {
+	
+	
+	/**
+	 * Esse método realiza o alojamento
+	 * do animal para adoção ou exterminio.
+	 */
+	public void alojar(){
+		System.out.println("Alojando cachorro");
+	}
+	
+	/**
+	 * Esse método realiza o extermínio de cachorro
+	 * @param nome do animal que será exterminado
+	 */
+	public void exterminarCachorro(String nome){
+		System.out.println("Exterminando Cachorro");
+	}
+
+}
---
+++ UsarCanil.java
@@ -0,0 +1,15 @@
+package br.unipe.java.unidade1;
+
+public class UsarCanil {
+
+	public static void main(String[] args) {
+		
+		Canil dungeon = new Canil();
+		
+		dungeon.alojar();
+		dungeon.exterminarCachorro("Iva");
+		
+
+	}
+
+}
