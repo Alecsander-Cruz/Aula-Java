44bc96a77b323f2ed3c72465052ca0c9a825d28b
Alecsander-Cruz <alecsandercruz@hotmail.com>
2017-05-10 21:28:04 -0300

    Add files via upload

---
+++ Erros/PrincipalContaCorrente.java
@@ -0,0 +1,30 @@
+/**
+ *
+ * @autor aluno
+ * @date 10 de mai de 2017
+ * @time 21:04:56
+ * ${tags}
+ * 
+ */
+package br.unipe.aula.java;
+
+/**
+ * @author aluno
+ *
+ */
+public class PrincipalContaCorrente {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+
+		Agencia agencia = new Agencia();
+		ContaCorrente conta = null;
+		
+		agencia.cadastrarConta(conta);
+
+	}
+
+}
