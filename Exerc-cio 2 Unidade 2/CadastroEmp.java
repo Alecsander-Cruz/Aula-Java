b2a83de7d5bfbe039ba578d4c58548f25dcdc7b5
Alecsander-Cruz <alecsandercruz@hotmail.com>
2017-06-07 13:54:18 -0300

    Update CadastroEmp.java

--- Exerc-cio 2 Unidade 2/CadastroEmp.java
+++ Exerc-cio 2 Unidade 2/CadastroEmp.java
@@ -1,9 +1,10 @@
-public class CadastroEmp extends Cliente {
+public class CadastroEmp {
 	
 	public float valorImovel;
 	public int funcionarios;
-	public int visitasDiarias ;
-	public String ramo;
+	public int visitasDiarias;
+	public int ramo; //1 - ramo Comercio; 2 - ramo Industria; 3 - ramo Agropecuaria
+	public float valorSeguroEmp;
 	
 	
 	public float getValorImovel() {
@@ -24,11 +25,40 @@ public class CadastroEmp extends Cliente {
 	public void setVisitasDiarias(int visitasDiarias) {
 		this.visitasDiarias = visitasDiarias;
 	}
-	public String getRamo() {
+	public int getRamo() {
 		return ramo;
 	}
-	public void setRamo(String ramo) {
+	public void setRamo(int ramo) {
 		this.ramo = ramo;
 	}
+	public float getValorSeguroEmp() {
+		return valorSeguroEmp;
+	}
+	public void setValorSeguroEmp(float valorSeguroEmp) {
+		this.valorSeguroEmp = valorSeguroEmp;
+	} 
 	
+	public float calcularSeguroEmp(float valorImovel, int funcionarios, int visitasDiarias, int ramo){
+		
+		float valorSeguro = 0;
+		int somaFuncionarios;
+		int somaVisitas;
+		
+		somaFuncionarios = (funcionarios/10);
+		somaVisitas = (visitasDiarias/200);
+		
+		if(ramo == 1){
+			valorSeguro = (valorImovel*4/100) + ((somaFuncionarios)*(valorImovel*2/1000)) + ((somaVisitas)*(valorImovel*3/1000)) + (valorImovel*5/1000);
+		}
+		
+		if(ramo == 2){
+			valorSeguro = (valorImovel*4/100) + ((somaFuncionarios)*(valorImovel*2/1000)) + ((somaVisitas)*(valorImovel*3/1000)) + (valorImovel*1/100);
+		}
+		
+		if(ramo == 3){
+			valorSeguro = (valorImovel*4/100) + ((somaFuncionarios)*(valorImovel*2/1000)) + ((somaVisitas)*(valorImovel*3/1000));
+		}
+		
+		return valorSeguro;
+	}
 }
